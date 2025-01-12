9d9885a74284f5085b55e23a7288c22b0f64cd9d
rishabhpreethan <rishabh.preethan@gmail.com>
2025-01-13 00:10:20 +0530

    deployed

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,12 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/f1/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
